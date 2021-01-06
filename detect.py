380f2446c350814f1b1b435bdd7df5051828c9c3
Ash-Sat0shi <yatabe.s@gmail.com>
2021-01-06 16:29:18 +0900

    [fix] wait for some other signals to prevent miss detection
    changed detect.py not to just send e-mail by detecting one signal.
    wait 6 continuous detection before sending e-mail.

--- detect.py
+++ detect.py
@@ -30,33 +30,6 @@ subject1 = "漏水検知システム　DetectPi-PROTOTYPE"
 subject2 = "停電検知システム　DetectPi-PROTOTYPE"
 subject3 = "タンク水位検知システム　DetectPi-PROTOTYPE"
 
-body1 = "発生時刻 : {0} \n 漏水を検知しました！！ \n RasPi IP Address is : {1} ".format(datetime.datetime.now(), myip)
-body2 = "発生時刻 : {0} \n 停電を検知しました！！ \n RasPi IP Address is : {1} ".format(datetime.datetime.now(), myip)
-body3 = "発生時刻 : {0} \n タンク水位上昇を検知しました！！ \n RasPi IP Address is : {1} ".format(datetime.datetime.now(), myip)
-
-
-msg1 = MIMEText(body1, "plain", "utf-8")
-        # prevent 'ascii' codec can't encode characters in position 0-14: ordinal not in range(128) というエラーがでる
-msg1["From"] = from_addr
-msg1["To"] = to_addr
-msg1["Date"] = formatdate()
-msg1["Subject"] = subject1
-
-msg2 = MIMEText(body2, "plain", "utf-8")
-        # prevent 'ascii' codec can't encode characters in position 0-14: ordinal not in range(128) というエラーがでる
-msg2["From"] = from_addr
-msg2["To"] = to_addr
-msg2["Date"] = formatdate()
-msg2["Subject"] = subject2
-
-msg3 = MIMEText(body3, "plain", "utf-8")
-        # prevent 'ascii' codec can't encode characters in position 0-14: ordinal not in range(128) というエラーがでる
-msg3["From"] = from_addr
-msg3["To"] = to_addr
-msg3["Date"] = formatdate()
-msg3["Subject"] = subject3
-
-
 # -- main --
 
 PIN_IN1 = 24
@@ -68,47 +41,85 @@ GPIO.setup(PIN_IN1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(PIN_IN2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(PIN_IN3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(PIN_OUT, GPIO.OUT)
+counter = 0
 
 try:
     while True:
         time.sleep(0.1)     # to avoid false positive at the first time python runs
         flag1 = GPIO.input(PIN_IN1) == GPIO.HIGH
-        GPIO.output(PIN_OUT, flag1)
+        flag2 = GPIO.input(PIN_IN2) == GPIO.HIGH
+        flag3 = GPIO.input(PIN_IN3) == GPIO.HIGH
+        
+        
+        
+
         if flag1 == True:
+            counter = counter+1
+            with open('log.txt', mode='a') as f:
+                f.write('発生時刻:' + str(datetime.datetime.now()) + '漏水を検知 回数：' + str(counter) + '\n')
             print("発生時刻:")
             print(datetime.datetime.now())
-            print("WATER DETECTED!! 漏水を検知しました！！")
-    # -------------------------------------------------------------------------------------------------------------
-	# send mail
-            try:
-                print("sending 漏水 mail...")
-                #send = smtplib.SMTP(SMTP, PORT)		# create SMTP object
-                #send.ehlo()
-                #send.starttls()
-                #send.ehlo()
-                #send.login(gmail_addr, gmail_pass)	# Login to Gmail
-                #send.send_message(msg1)
-                #send.close()
-            except Exception as e:
-                print("except: " + str(e))		# in case of error
-            else:
-                print("Successfully sent WATER LEAKAGE mail to {0}".format(to_addr))	# when succeed
-                time.sleep(3)
-     # -------------------------------------------------------------------------------------------------------------
-        
-        else:
-            print("NO WATER LEAKAGE")
-            
-            msg2 = MIMEText(body2, "plain", "utf-8")
-            flag2 = GPIO.input(PIN_IN2) == GPIO.HIGH
-            GPIO.output(PIN_OUT, flag2)
-            if flag2 == True:
-                print("発生時刻:")
-                print(datetime.datetime.now())
-                print(" BLACKOUT DETECTED!! 停電を検知しました！！")
-                # -------------------------------------------------------------------------------------------------------------
-                # send mail
+            print("WATER DETECTED!! 漏水を検知しました！！ time : {0}".format(datetime.datetime.now))
+            body1 = "発生時刻 : {0} \n 漏水を検知しました！！ \n RasPi IP Address is : {1} ".format(datetime.datetime.now(), myip)
+            msg1 = MIMEText(body1, "plain", "utf-8")
+        # prevent 'ascii' codec can't encode characters in position 0-14: ordinal not in range(128) というエラーがでる
+            msg1["From"] = from_addr
+            msg1["To"] = to_addr
+            msg1["Date"] = formatdate()
+            msg1["Subject"] = subject1
+            time.sleep(1)
+
+            # -------------------------------------------------------------------------------------------------------------
+            # send mail
+            if counter == 5:
+                
                 try:
+                    GPIO.output(PIN_OUT, flag1)
+                    print("sending 漏水 mail...")
+                    with open('log.txt', mode='a') as f:
+                        f.write('送信時刻:' + str(datetime.datetime.now()) + 'メール送信中…')
+                    # send = smtplib.SMTP(SMTP, PORT)		# create SMTP object
+                    # send.ehlo()
+                    # send.starttls()
+                    # send.ehlo()
+                    # send.login(gmail_addr, gmail_pass)	# Login to Gmail
+                    # send.send_message(msg1)
+                    # send.close()
+                    counter = 0
+                    
+                except Exception as e:
+                    print("except: " + str(e))		# in case of error
+                    with open('log.txt', mode='a') as f:
+                        f.write('メール送信エラー' + str(e) + '\n')
+                else:
+                    print("Successfully sent WATER LEAKAGE mail to {0}".format(to_addr))	# when succeed
+                    with open('log.txt', mode='a') as f:
+                        f.write('……メール送信完了' + '\n')
+                    print(body1)
+                    time.sleep(10)
+                    GPIO.output(PIN_OUT, 0)
+            # -------------------------------------------------------------------------------------------------------------
+        elif flag2 == True:
+            counter = counter+1
+            with open('log.txt', mode='a') as f:
+                f.write('発生時刻:' + str(datetime.datetime.now()) + '停電を検知 回数：' + str(counter) + '\n')
+            print("発生時刻:")
+            print(datetime.datetime.now())
+            print(" BLACKOUT DETECTED!! 停電を検知しました！！ time : {0}".format(datetime.datetime.now))
+            body2 = "発生時刻 : {0} \n 停電を検知しました！！ \n RasPi IP Address is : {1} ".format(datetime.datetime.now(), myip)
+            msg2 = MIMEText(body2, "plain", "utf-8")
+        # prevent 'ascii' codec can't encode characters in position 0-14: ordinal not in range(128) というエラーがでる
+            msg2["From"] = from_addr
+            msg2["To"] = to_addr
+            msg2["Date"] = formatdate()
+            msg2["Subject"] = subject2
+            
+            time.sleep(1)
+            # -------------------------------------------------------------------------------------------------------------
+            # send mail
+            if counter == 20:
+                try:
+                    GPIO.output(PIN_OUT, flag2)
                     print("sending 停電 mail...")
                     #send = smtplib.SMTP(SMTP, PORT)		# create SMTP object
                     #send.ehlo()
@@ -121,39 +132,49 @@ try:
                     print("except: " + str(e))		# in case of error
                 else:
                     print("Successfully sent BLACKOUT mail to {0}".format(to_addr))	# when succeed
-                    time.sleep(3)
-                time.sleep(3)
+                    print(body2)
+                    GPIO.output(PIN_OUT, 0)
                 # -------------------------------------------------------------------------------------------------------------
-            else:
-                print("NO BLACKOUT")
-                
-                msg3 = MIMEText(body3, "plain", "utf-8")
-                flag3 = GPIO.input(PIN_IN3) == GPIO.HIGH
-                GPIO.output(PIN_OUT, flag3)
-            
-                if flag3 == True:
-                    print("発生時刻:")
-                    print(datetime.datetime.now())
-                    print(" TANK LEVEL RISING DETECTED!! 水位上昇を検知しました！！")
-                    # -------------------------------------------------------------------------------------------------------------
-                    # send mail
-                    try:
-                        print("sending 水位上昇 mail...")
-                        #send = smtplib.SMTP(SMTP, PORT)		# create SMTP object
-                        #send.ehlo()
-                        #send.starttls()
-                        #send.ehlo()
-                        #send.login(gmail_addr, gmail_pass)	# Login to Gmail
-                        #send.send_message(msg3)
-                        #send.close()
-                    except Exception as e:
-                        print("except: " + str(e))		# in case of error
-                    else:
-                        print("Successfully sent TANK LEVEL RISING mail to {0}".format(to_addr))	# when succeed
-                        time.sleep(3)
+        elif flag3 == True:
+            counter = counter+1
+            with open('log.txt', mode='a') as f:
+                f.write('発生時刻:' + str(datetime.datetime.now()) + 'タンク水位上昇を検知 回数：' + str(counter) + '\n')
+            print("発生時刻:")
+            print(datetime.datetime.now())
+            print(" TANK LEVEL RISING DETECTED!! 水位上昇を検知しました！！ time : {0}".format(datetime.datetime.now))
+            body3 = "発生時刻 : {0} \n タンク水位上昇を検知しました！！ \n RasPi IP Address is : {1} ".format(datetime.datetime.now(), myip)
+            msg3 = MIMEText(body3, "plain", "utf-8")
+        # prevent 'ascii' codec can't encode characters in position 0-14: ordinal not in range(128) というエラーがでる
+            msg3["From"] = from_addr
+            msg3["To"] = to_addr
+            msg3["Date"] = formatdate()
+            msg3["Subject"] = subject3
+            # -------------------------------------------------------------------------------------------------------------
+            # send mail
+            if counter == 20:
+                try:
+                    GPIO.output(PIN_OUT, flag3)
+                    print("sending 水位上昇 mail...")
+                    #send = smtplib.SMTP(SMTP, PORT)		# create SMTP object
+                    #send.ehlo()
+                    #send.starttls()
+                    #send.ehlo()
+                    #send.login(gmail_addr, gmail_pass)	# Login to Gmail
+                    #send.send_message(msg3)
+                    #send.close()
+                except Exception as e:
+                    print("except: " + str(e))		# in case of error
                 else:
-                    print("NO TANK LEVEL RISING")
-        time.sleep(3)
+                    print("Successfully sent TANK LEVEL RISING mail to {0}".format(to_addr))	# when succeed
+                    print(body3)
+                    time.sleep(1)
+                    GPIO.output(PIN_OUT, 0)
+        else:
+            print("ALL GREEN")
+            GPIO.output(PIN_OUT, 0)
+            time.sleep(1)
+            counter = 0
+                    
 except KeyboardInterrupt:
     pass
 finally:
